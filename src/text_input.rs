e0432a7ebeb41580657fafeac86aada7ea784408
ax-lab <98651633+ax-lab@users.noreply.github.com>
2022-04-15 21:08:15 -0300

    feat(input): change `TextInput` to peek/advance
    Change the trait to support peeking chars without consuming the input.
    
    - change `read_chars` to `peek_chars`
    - add `advance` method

--- src/text_input.rs
+++ src/text_input.rs
@@ -1,12 +1,16 @@
 /// Provides an abstraction for text input that supports reading a string
 /// with the specified number of characters at a time.
 pub trait TextInput {
-	/// Read the next `count` characters from the input and return them as a
-	/// string slice.
+	/// Returns the next `count` characters from the input as a string slice
+	/// without consuming the input.
 	///
 	/// At the end of the input, this may return less than `count` characters
 	/// (including the empty string).
-	fn read_chars(&mut self, count: usize) -> &str;
+	fn peek_chars(&mut self, count: usize) -> &str;
+
+	/// Consumes the next `count` characters from the input, stopping at the
+	/// end of the input if there is not enough characters left.
+	fn advance(&mut self, count: usize);
 }
 
 /// Implementation of [`TextInput`] for a borrowed [`str`] slice.
@@ -25,10 +29,14 @@ impl<'a> StrInput<'a> {
 }
 
 impl<'a> TextInput for StrInput<'a> {
-	fn read_chars(&mut self, count: usize) -> &str {
+	fn peek_chars(&mut self, count: usize) -> &str {
 		let output = take_chars_from(self.source, self.offset, count);
-		self.offset += output.len();
 		output
+	}
+
+	fn advance(&mut self, count: usize) {
+		let text = self.peek_chars(count);
+		self.offset += text.len();
 	}
 }
 
@@ -48,10 +56,14 @@ impl StringInput {
 }
 
 impl TextInput for StringInput {
-	fn read_chars(&mut self, count: usize) -> &str {
+	fn peek_chars(&mut self, count: usize) -> &str {
 		let output = take_chars_from(self.source.as_str(), self.offset, count);
-		self.offset += output.len();
 		output
+	}
+
+	fn advance(&mut self, count: usize) {
+		let text = self.peek_chars(count);
+		self.offset += text.len();
 	}
 }
 
@@ -62,6 +74,7 @@ where
 {
 	source: T,
 	buffer: String,
+	buffer_count: usize,
 }
 
 impl<T> CharsInput<T>
@@ -72,6 +85,7 @@ where
 		CharsInput {
 			source: input,
 			buffer: Default::default(),
+			buffer_count: 0,
 		}
 	}
 }
@@ -80,20 +94,33 @@ impl<T> TextInput for CharsInput<T>
 where
 	T: Iterator<Item = char>,
 {
-	fn read_chars(&mut self, count: usize) -> &str {
-		self.buffer.clear();
-
-		let mut read = 0;
-		while read < count {
+	fn peek_chars(&mut self, count: usize) -> &str {
+		while self.buffer_count < count {
 			if let Some(char) = self.source.next() {
 				self.buffer.push(char);
-				read += 1;
+				self.buffer_count += 1;
 			} else {
 				break;
 			}
 		}
 
-		self.buffer.as_str()
+		let buffer = self.buffer.as_str();
+		if count < self.buffer_count {
+			take_chars_from(buffer, 0, count)
+		} else {
+			buffer
+		}
+	}
+
+	fn advance(&mut self, count: usize) {
+		let advance_len = self.peek_chars(count).len();
+		if count < self.buffer_count {
+			self.buffer_count -= count;
+			self.buffer.replace_range(0..advance_len, "");
+		} else {
+			self.buffer.clear();
+			self.buffer_count = 0;
+		}
 	}
 }
 
@@ -122,7 +149,97 @@ mod test_text_input {
 	use super::StringInput;
 	use super::TextInput;
 
-	macro_rules! check {
+	#[test]
+	fn from_str() {
+		let mut input = StrInput::new("abc");
+		assert_eq!(input.peek_chars(3), "abc");
+	}
+
+	#[test]
+	fn from_string() {
+		let mut input = StringInput::new(String::from("123"));
+		assert_eq!(input.peek_chars(3), "123");
+	}
+
+	#[test]
+	fn from_iterator() {
+		let mut input = CharsInput::new("xyz".chars());
+		assert_eq!(input.peek_chars(3), "xyz");
+	}
+
+	#[test]
+	fn peek_does_not_consume_input() {
+		fn check<T: TextInput>(mut input: T, name: &str) {
+			assert_eq!(input.peek_chars(1), "1", "in {}", name);
+			assert_eq!(input.peek_chars(3), "123", "in {}", name);
+			assert_eq!(input.peek_chars(2), "12", "in {}", name);
+			assert_eq!(input.peek_chars(1), "1", "in {}", name);
+		}
+		check(StrInput::new("123"), "StrInput");
+		check(StringInput::new("123".into()), "StringInput");
+		check(CharsInput::new("123".chars()), "CharsInput");
+	}
+
+	#[test]
+	fn advance_consumes_input() {
+		fn check<T: TextInput>(mut input: T, name: &str) {
+			// scenarios:
+			// 1) read then advance
+			// 2) advance then read
+			// 3) advance within read buffer
+			// 4) advance across read buffer
+			// 5) advance partially then read across buffer
+			// 6) advance to end
+			// 7) advance past end
+
+			// 1
+			assert_eq!(input.peek_chars(1), "1", "in {}", name);
+			input.advance(1);
+			assert_eq!(input.peek_chars(2), "23", "in {}", name);
+			input.advance(2);
+			assert_eq!(input.peek_chars(1), "4", "in {}", name);
+			input.advance(1);
+
+			// 2
+			input.advance(1);
+			assert_eq!(input.peek_chars(1), "6", "in {}", name);
+			input.advance(1);
+
+			// 3
+			assert_eq!(input.peek_chars(4), "7890", "in {}", name);
+			input.advance(1);
+			assert_eq!(input.peek_chars(2), "89", "in {}", name);
+			input.advance(2);
+			assert_eq!(input.peek_chars(1), "0", "in {}", name);
+			input.advance(1);
+
+			// 4
+			assert_eq!(input.peek_chars(1), "A", "in {}", name);
+			input.advance(2);
+			assert_eq!(input.peek_chars(1), "C", "in {}", name);
+			input.advance(1);
+
+			// 5
+			assert_eq!(input.peek_chars(2), "DE", "in {}", name);
+			input.advance(1);
+			assert_eq!(input.peek_chars(2), "EF", "in {}", name);
+			input.advance(2);
+
+			// 6 and 7
+			assert_eq!(input.peek_chars(1), "!", "in {}", name);
+			input.advance(1);
+			assert_eq!(input.peek_chars(1), "", "in {}", name);
+			input.advance(1);
+			assert_eq!(input.peek_chars(1), "", "in {}", name);
+		}
+
+		const INPUT: &'static str = "1234567890ABCDEF!";
+		check(StrInput::new(INPUT), "StrInput");
+		check(StringInput::new(INPUT.into()), "StringInput");
+		check(CharsInput::new(INPUT.chars()), "CharsInput");
+	}
+
+	macro_rules! check_reads {
 		($input:expr, $( $count:expr => $expected:expr ),*) => {
 			let mut input1 = StrInput::new($input);
 			let mut input2 = StringInput::new($input.to_string());
@@ -131,65 +248,51 @@ mod test_text_input {
 				let count = $count;
 				let expected = $expected;
 
-				let output = input1.read_chars(count);
+				let output = input1.peek_chars(count);
 				assert_eq!(output, expected,
 					"StrInput.read({}): expected `{}`, got `{}`",
 					count, expected, output);
 
-				let output = input2.read_chars(count);
+				let output = input2.peek_chars(count);
 				assert_eq!(output, expected,
 					"StringInput.read({}): expected `{}`, got `{}`",
 					count, expected, output);
 
-				let output = input3.read_chars(count);
+				let output = input3.peek_chars(count);
 				assert_eq!(output, expected,
 					"CharsInput.read({}): expected `{}`, got `{}`",
 					count, expected, output);
+
+				input1.advance(count);
+				input2.advance(count);
+				input3.advance(count);
 			})+
 		};
 	}
 
 	#[test]
-	fn from_str() {
-		let mut input = StrInput::new("abc");
-		assert_eq!(input.read_chars(3), "abc");
-	}
-
-	#[test]
-	fn from_string() {
-		let mut input = StringInput::new(String::from("123"));
-		assert_eq!(input.read_chars(3), "123");
-	}
-
-	#[test]
-	fn from_iterator() {
-		let mut input = CharsInput::new("xyz".chars());
-		assert_eq!(input.read_chars(3), "xyz");
-	}
-
-	#[test]
 	fn with_empty_input() {
-		check!("", 1 => "");
+		check_reads!("", 1 => "");
 	}
 
 	#[test]
 	fn can_read_whole_input() {
-		check!("input", 5 => "input");
+		check_reads!("input", 5 => "input");
 	}
 
 	#[test]
 	fn returns_empty_at_end_of_input() {
-		check!("123", 3 => "123", 3 => "", 1 => "");
+		check_reads!("123", 3 => "123", 3 => "", 1 => "");
 	}
 
 	#[test]
 	fn returns_count_characters() {
-		check!("12345", 1 => "1", 1 => "2", 2 => "34", 1 => "5");
-		check!("12345", 3 => "123", 3 => "45");
+		check_reads!("12345", 1 => "1", 1 => "2", 2 => "34", 1 => "5");
+		check_reads!("12345", 3 => "123", 3 => "45");
 	}
 
 	#[test]
 	fn supports_multibyte_characters() {
-		check!("日本語が上手ですね", 2 => "日本", 1 => "語", 1 => "が", 2 => "上手", 3 => "ですね");
+		check_reads!("日本語が上手ですね", 2 => "日本", 1 => "語", 1 => "が", 2 => "上手", 3 => "ですね");
 	}
 }
